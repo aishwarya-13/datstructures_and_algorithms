f4b4aefab6c8d6e67ed0b11b29a97f3a02d7a4d6
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-09-17 17:36:24 -0500

    Valid palindrome

--- Arrays/Two Pointers/Easy/Valid Palindrome.js
+++ Arrays/Two Pointers/Easy/Valid Palindrome.js
@@ -1,6 +1,38 @@
 /**
  * https://leetcode.com/problems/valid-palindrome/
  */
+
+/**
+    Without using regular expression
+ */
+var isPalindrome = function(s) {
+    let left = 0,
+        right = s.length - 1,
+        str = s.toLowerCase();
+    while(left <= right){
+        if(isChar(str[left]) && isChar(str[right])){
+            if(str[left] === str[right]){
+                left++;
+                right--;
+            }else{
+                return false
+            }
+        }else if(!isChar(str[left]) && isChar(str[right])){
+            left++
+        }else{
+            right--
+        }
+    }
+    return true
+};
+
+const isChar = (char)=>{
+    if((char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) || //its an alphabet
+        (char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57)){//its a number
+        return true
+    }
+    return false
+}
  /**
     Time: O(N)
     Space: O(N) -> to store modified string ???
