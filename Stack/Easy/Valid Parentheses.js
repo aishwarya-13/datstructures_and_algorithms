3fbc720ea8ca23fd7305b58ed67d282f03dae8bb
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-11 22:07:27 -0500

    Valid Parentheses

--- Stack/Easy/Valid Parentheses.js
+++ Stack/Easy/Valid Parentheses.js
@@ -3,6 +3,24 @@
  */
 
 /**
+    Time: O(N) - We traverse the string one character at a time and push, pop and peek operations are O(1)
+    Space: O(N) 
+ */
+    var isValid = (str)=>{
+        let stack = [];
+        for(let i=0; i<str.length; i++){
+            if(str[i] === ')' && stack[stack.length - 1] !== '(' ||
+                str[i] === ']' && stack[stack.length - 1] !== '[' ||
+                str[i] === '}' && stack[stack.length - 1] !== '{'){
+                    stack.pop()
+            }else{
+                stack.push(str[i])
+            }
+        }
+        return stack.length === 0
+    };
+
+/**
     Push opening brace onto stack
     If a closing brace then pop and check for the type. If not same type return false.
     Time: O(N)
