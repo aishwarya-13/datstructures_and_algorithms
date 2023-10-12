7656880298fb2b83a710b2d84ca4270e467e6a91
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-11 22:33:29 -0500

    Backspace String Compare

--- Stack/Easy/Backspace String Compare.js
+++ Stack/Easy/Backspace String Compare.js
@@ -1,6 +1,30 @@
 /**
  * https://leetcode.com/problems/backspace-string-compare/
  */
+
+//Using stack
+var backspaceCompare = function(s, t) {
+    let stack1 = [],
+        stack2 = [],
+        i = 0,
+        j = 0;
+    while(i<s.length || j<t.length){
+        if(s[i] === '#'){
+            stack1.pop(s[i])
+        }else{
+            stack1.push(s[i])
+        }
+        if(t[j] === '#'){
+            stack2.pop(t[j])
+        }else{
+            stack2.push(t[j])
+        }
+        i++
+        j++
+    }
+    return stack1.join('') === stack2.join('')
+};
+
 var backspaceCompare = function(s, t) {
   let stack1 = [],
       stack2 = [];
