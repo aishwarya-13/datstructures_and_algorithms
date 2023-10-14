1cb7b078c937bf56bf6e697235c80b438778ccb6
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-14 15:33:32 -0500

    Min diff between highest and lowest of k scores

--- Sliding Window/Easy/Minimum Difference Between Highest and Lowest of K Scores.js
+++ Sliding Window/Easy/Minimum Difference Between Highest and Lowest of K Scores.js
@@ -1,6 +1,23 @@
 /**
  * https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/
  */
+/**
+ * Without using extra variable for counting 
+ */
+var minimumDifference = function(nums, k) {
+    nums.sort((a,b)=> a-b)
+    let result = +Infinity,
+        start = 0;
+    for(let end =0; end<nums.length; end++){
+        k--
+        if(k === 0){
+            result = Math.min(result, (nums[end] - nums[start]))
+            start++
+            k++
+        }
+    }
+    return result
+};
 
 var minimumDifference = function(nums, k) {
   nums.sort((a,b)=> a-b)
