aca888ea34eae7d5965cddc240a32e3f44eaf081
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-14 20:33:26 -0500

    Longest susbtring with atmost k distinct characters

--- Sliding Window/Medium/Longest Substring with At Most K Distinct Characters.js
+++ Sliding Window/Medium/Longest Substring with At Most K Distinct Characters.js
@@ -3,6 +3,33 @@
  */
 
 /**
+ * Without using mapLen
+ */
+var lengthOfLongestSubstringKDistinct = function(s, k) {
+    let map = {},
+        result =0,
+        start=0;
+    for(let end=0; end<s.length; end++){
+        let char = s[end]
+        if(map[char] === undefined){
+            map[char] = 0
+            k--
+        }
+        map[char]++
+        if(k<0){
+            map[s[start]]--
+            if(map[s[start]] === 0){
+                delete map[s[start]]
+                k++
+            }
+            start++
+        }
+        result = Math.max(result, (end - start + 1))
+    }
+    return result
+};
+
+/**
     Aishwarya
 Time: Outer for loop runs for O(N) time. Inner while loop processes each character only once (it will remove the char from start position)
  So inner while loop will run for O(N) time.
