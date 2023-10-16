053ae2fc70799f18f76581019c0c63ea6aede6ef
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-15 22:39:57 -0500

    Find all anagrams in a string

--- Sliding Window/Medium/Find all Anagrams in a String.js
+++ Sliding Window/Medium/Find all Anagrams in a String.js
@@ -6,43 +6,47 @@
     Here window will be equal to the length of pattern
  */
     var findAnagrams = function(str, pattern) {
-      let map = {},
-          mapLen = 0,
-          start = 0,
-          matched = 0,
-          result = [];
-      //Calculate frequency of chars in pattern
-      for(let p of pattern){
-          if(map[p] === undefined){
-              map[p] = 0
-              mapLen++
-          }
-          map[p]++
-      }
-      for(let end=0; end<str.length; end++){
-          let char = str[end]
-          if(map[char] !== undefined){
-              map[str[end]]--
-          }
-          if(map[char] === 0){//found a char match
-              matched++//count the frequncies of chars being matched
-          }
-          if(matched === mapLen){//found a anagram
-              result.push(start)
-          }
-          //if window size exceeds
-          if(end >= pattern.length - 1){
-              if(map[str[start]] !== undefined){
-                  if(map[str[start]] === 0){
-                      matched--
-                  }
-                  map[str[start]]++//add char back in the map
-              }
-              start++//slide window ahead
-          }
-      }
-      return result;
-  };
+        let map = {},
+            mapLen = 0,
+            start = 0,
+            matched = 0,
+            result = [];
+        //Create frequency of chars in map
+        for(let char of pattern){
+            if(map[char] === undefined){
+                map[char] = 0
+                mapLen++
+            }
+            map[char]++
+        }
+        //Iterate str
+        for(let end=0; end<str.length; end++){
+            let char = str[end]
+            //found a char in map so decease the freq
+            if(map[char] !== undefined){
+                map[char]--
+            }
+            //found all freq of a char so got one char match
+            if(map[char] === 0){
+                matched++
+            }
+            //found all chars match
+            if(matched === mapLen){
+                result.push(start)
+            }
+            //here window size will be equal to pattern length
+            //so when end index exceeds pattern length - start sliding the window
+            if(end >= pattern.length - 1){
+                let ch = str[start]
+                if(map[ch] === 0){
+                    matched--
+                }
+                map[ch]++//add char back in map
+                start++//slide the window
+            }
+        }
+        return result
+    };
 
 /**
  * Footnote: Find all anagrams, Permutation in a string and Min Window substring have similar solution
