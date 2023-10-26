204956747358ba198c277a8607d9d4af227e6211
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-25 22:29:35 -0500

    Merge two sorted lists

--- Linked Lists/Easy/Merge Two Sorted Lists.js
+++ Linked Lists/Easy/Merge Two Sorted Lists.js
@@ -3,6 +3,51 @@
  */
 
 /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+function ListNode(val, next){
+    this.val = val || 0;
+    this.next = next || null;
+}
+ */
+
+/**
+1]Create a dummy pointer that will iterate the result list
+2]Iterate list1 and list2
+3]Attch the node with lesser value to dummy and move that pointer ahead
+4]At end of iteration, append the remaining list.
+
+Time: O(N+M)
+Space: O(1) it allocates a few pointers so has constant overall space
+**/
+function mergeTwoLists(list1, list2) {
+    let dummy = new ListNode(-1),
+        result = dummy;
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            dummy.next = list1;
+            list1 = list1.next
+        }else{
+            dummy.next = list2;
+            list2 = list2.next
+        }
+        dummy = dummy.next
+    }
+    if(list1 === null){
+        dummy.next = list2
+    }else{
+        dummy.next = list1
+    }
+    return result.next
+}
+
+/**
 function ListNode(val, next){
     this.val = val || 0;
     this.next = next || null;
