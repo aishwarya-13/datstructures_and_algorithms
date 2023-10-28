0b2b3b833c20cdf268b543db6cce86056a44dfef
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-28 18:13:22 -0500

    Add Two Numbers II

--- Linked Lists/Medium/Add Two Numbers II.js
+++ Linked Lists/Medium/Add Two Numbers II.js
@@ -1,5 +1,9 @@
 /**
- * https://leetcode.com/problems/add-two-numbers-ii/
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
 
 /**
@@ -12,39 +16,48 @@ Space:O(N)
 **/
 var addTwoNumbers = function(list1, list2) {
     let l1 = reverse(list1),
-        l2 = reverse(list2),
+        l2 = reverse(list2);
+    let sumList = addTwoLists(l1, l2);
+    let resultList = reverse(sumList)
+    return resultList;
+};
+
+const addTwoLists = (l1, l2) =>{
+    let dummy = new ListNode(-1),
+        result = dummy,
         sum = 0,
-        result = null;
-     console.log(list1, list2)
-     while(l1 || l2){
-         if(l1){
-             sum+= l1.val;
-             l1 = l1.next;
-         }
-         if(l2){
-             sum+= l2.val;
-             l2 = l2.next;
-         }
-         let node = new ListNode(sum % 10, result);
-         result = node;
-         sum = sum > 9 ? 1 : 0;
-     }
-     if(sum){
-         result = new ListNode(1, result);
-     }
-     return result;
- };
- 
- const reverse = (list)=>{
-     let prev = null,
-         curr = list;
-     while(curr){
-         let saveNext = curr.next;
-         curr.next = prev;
-         prev = curr;
-         curr = saveNext;
-     }
-     return prev;
- }
- 
- 
+        carry = 0;
+    while(l1 || l2){
+        if(l1 && l2){
+            sum = l1.val + l2.val + carry
+            l1 = l1.next
+            l2 = l2.next
+        }else if(l1 && !l2){
+            sum = l1.val + carry
+            l1 = l1.next
+        }else if(!l1 && l2){
+            sum = l2.val + carry
+            l2 = l2.next
+        }
+        carry = Math.floor(sum / 10),
+        dummy.next = new ListNode(sum % 10)
+        dummy = dummy.next
+    }
+    if(carry){
+        dummy.next = new ListNode(carry)
+    }
+    return result.next
+}
+
+const reverse = (list)=>{
+    let curr = list,
+        prev = null;
+    while(curr){
+        let saveNext = curr.next;
+        curr.next = prev
+        prev = curr
+        curr = saveNext
+    }
+    return prev
+}
+
