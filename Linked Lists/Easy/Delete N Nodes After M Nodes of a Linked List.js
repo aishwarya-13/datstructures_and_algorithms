339d079d056d00832c6f2266bc38ca0b7a798938
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-10-28 14:27:08 -0500

    Delete N nodes after M nodes

--- Linked Lists/Easy/Delete N Nodes After M Nodes of a Linked List.js
+++ Linked Lists/Easy/Delete N Nodes After M Nodes of a Linked List.js
@@ -12,6 +12,45 @@
  5]Reset the counters
   */
  var deleteNodes = function(head, m, n) {
+    let keep = m,
+        del = n,
+        node = head;
+    while(node){
+        if(keep === 1){
+            let prev = node //keep reference of mth node to make new link
+            //move n nodes ahead
+            while(node && del >= 0){
+                node = node.next
+                del--
+            }
+            //make new links
+            if(node === null){
+                prev.next = null
+                break;
+            }else{
+                prev.next = node
+            }
+            //reset pointers
+            del = n
+            keep = m
+            continue
+        }
+        keep--
+        node = node.next
+    }
+    return head;
+};
+
+ /**
+ Aishwarya
+ 
+ 1]Keep two variables m and n that hold the number of nodes to be deleted and kept
+ 2]Keep a previous variable that makes new linkages
+ 3]Iterate till mth position save it in a prev variable
+ 4]Iterate till nth position and make new connection with the prev variable
+ 5]Reset the counters
+  */
+ var deleteNodes = function(head, m, n) {
     let prev = null,
         keep = m,
         del = n,
