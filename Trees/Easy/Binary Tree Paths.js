326d6d58d61ed8a2f45c01bd5f80debdc1239ad9
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-09 13:42:53 -0700

    Binary tree paths

--- Trees/Easy/Binary Tree Paths.js
+++ Trees/Easy/Binary Tree Paths.js
@@ -2,22 +2,20 @@
  * https://leetcode.com/problems/binary-tree-paths/
  */
 
-//Top down
-var binaryTreePaths = function(root) {
-    let result = [];
-    const dfs = (root, str)=>{
-        if(!root){
-            return;
-        }
-        if(!root.left && !root.right){
-            str+=`${root.val}`;
-            result.push(str);
-            return;
-        }
-        str += `${root.val}->`;
-        dfs(root.left, str);
-        dfs(root.right, str);
-    };
-    dfs(root, '');
-    return result;
+//Using pre-order
+var binaryTreePaths = function (root) {
+  const result = [];
+  const dfs = (node, str) => {
+    if (!node) {
+      return str;
+    }
+    str = str ? `${str}->${node.val}` : `${node.val}`;
+    if (!node.left && !node.right) {
+      result.push(str);
+    }
+    dfs(node.left, str);
+    dfs(node.right, str);
+  };
+  dfs(root, "");
+  return result;
 };
