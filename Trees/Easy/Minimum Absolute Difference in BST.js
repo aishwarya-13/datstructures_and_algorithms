d0179b09e3a128723eada17d8f5225a1420b02bf
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-09 14:49:10 -0700

    Min absolute diff in BST

--- Trees/Easy/Minimum Absolute Difference in BST.js
+++ Trees/Easy/Minimum Absolute Difference in BST.js
@@ -1,24 +1,45 @@
 /**
  * https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+ * https://leetcode.com/problems/minimum-distance-between-bst-nodes/
  */
 
-const getMinimumDifference = (root)=>{
-    let min = +Infinity,
-        curr = null;
-    const dfs = (root)=>{
-        if(!root){
-            return;
-        }
-        dfs(root.left);
-        if(curr === null){
-            curr = root.val;
-        }else{
-           let diff = Math.abs(root.val - curr);
-           min = Math.min(min, diff);
-           curr = root.val;
-        }
-        dfs(root.right);
+var minDiffInBST = function (root) {
+  let result = Infinity;
+  let prev = Infinity;
+  const dfs = (node) => {
+    if (!node) {
+      return;
     }
-    dfs(root);
-    return min;
-}
+    dfs(node.left);
+    console.log("node", node.val, "prev", prev);
+    let diff = Math.abs(prev - node.val);
+    if (diff < result) {
+      result = diff;
+    }
+    prev = node.val;
+    dfs(node.right);
+  };
+  dfs(root);
+  return result;
+};
+
+const getMinimumDifference = (root) => {
+  let min = +Infinity,
+    curr = null;
+  const dfs = (root) => {
+    if (!root) {
+      return;
+    }
+    dfs(root.left);
+    if (curr === null) {
+      curr = root.val;
+    } else {
+      let diff = Math.abs(root.val - curr);
+      min = Math.min(min, diff);
+      curr = root.val;
+    }
+    dfs(root.right);
+  };
+  dfs(root);
+  return min;
+};
---
+++ Trees/Notes.md
@@ -0,0 +1,17 @@
+## How to store previous value in BST
+
+var minDiffInBST = function(root) {
+const result = Infinity
+let prev = -Infinity
+const dfs = (node) =>{
+if(!node){
+return
+}
+dfs(node.left)
+console.log('node', node.val, 'prev', prev)
+prev = node.val
+dfs(node.right)
+}
+dfs(root)
+return result
+};
