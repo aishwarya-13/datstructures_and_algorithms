62e2018ef2753facb0fc8fd81eef41765cf89f2d
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-09 14:18:10 -0700

    Same tree

--- Trees/Easy/Same Tree.js
+++ Trees/Easy/Same Tree.js
@@ -2,16 +2,40 @@
  * https://leetcode.com/problems/same-tree/
  */
 
-//Recursive Bottom Up
-var isSameTree = function(p, q) {
-    if(p === null && q === null){
-        return true;
+var isSameTree = function (p, q) {
+  const dfs = (treeNode1, treeNode2) => {
+    /**
+            Why return true here?
+            Because: Till the point it reaches null node the false conditions did not occur
+            that means it is true 
+         */
+    if (!treeNode1 && !treeNode2) {
+      return true;
     }
-    if(!p || !q || p.val !== q.val){
-        return false;
+    if (
+      (!treeNode1 && treeNode2) || //if tree1 has one either left node or right node null
+      (treeNode1 && !treeNode2) || //if tree2 has one either left node or right node null
+      treeNode1.val !== treeNode2.val
+    ) {
+      return false;
     }
-    let left = isSameTree(p.left, q.left),
-        right = isSameTree(p.right, q.right),
-        val = p.val === q.val;
-    return left && right && val;
+    return (
+      dfs(treeNode1.left, treeNode2.left) &&
+      dfs(treeNode1.right, treeNode2.right)
+    );
+  };
+  return dfs(p, q);
+};
+
+var isSameTree = function (p, q) {
+  if (p === null && q === null) {
+    return true;
+  }
+  if (!p || !q || p.val !== q.val) {
+    return false;
+  }
+  let left = isSameTree(p.left, q.left),
+    right = isSameTree(p.right, q.right),
+    val = p.val === q.val;
+  return left && right && val;
 };
