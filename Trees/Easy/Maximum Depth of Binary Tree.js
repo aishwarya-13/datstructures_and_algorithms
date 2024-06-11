e6cac278ebd369847d3a38762e409c1bfd5e1798
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-10 22:07:30 -0700

    MAx depth of binary tree

--- Trees/Easy/Maximum Depth of Binary Tree.js
+++ Trees/Easy/Maximum Depth of Binary Tree.js
@@ -2,12 +2,33 @@
  * https://leetcode.com/problems/maximum-depth-of-binary-tree/
  */
 
-//Approach 1 -Recursive Bottom Up
-const maxDepth = (root)=>{
-    if(!root){
-        return 0;
+/**
+ * Top down
+ */
+var maxDepth = function (root) {
+  let max = 0;
+  const dfs = (node, count) => {
+    if (!node) {
+      return;
     }
-    const left = maxDepth(root.left);
-    const right = maxDepth(root.right);
-    return Math.max(left, right) + 1;
-};   
+    if (!node.left && !node.right) {
+      if (count > max) {
+        max = count;
+      }
+    }
+    dfs(node.left, count + 1);
+    dfs(node.right, count + 1);
+  };
+  dfs(root, 1);
+  return max;
+};
+
+//Approach 1 -Recursive Bottom Up
+const maxDepth = (root) => {
+  if (!root) {
+    return 0;
+  }
+  const left = maxDepth(root.left);
+  const right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
+};
