e0e9e7a01a42e6b78ae0421ef7ea0010f5edc4ae
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-14 21:13:27 -0700

    Diameter of BT

--- Trees/Easy/ Diameter of Binary Tree.js
+++ Trees/Easy/ Diameter of Binary Tree.js
@@ -2,17 +2,21 @@
  * https://leetcode.com/problems/diameter-of-binary-tree/
  */
 
-var diameterOfBinaryTree = function(root) {
-    let len = 0;
-    const dfs = (root)=>{
-        if(!root){
-            return 0;
-        }
-        const leftLength = dfs(root.left);//Get left path length
-        const rightLength = dfs(root.right);//Get right path length
-        len = Math.max(len, (leftLength + rightLength));//see if we found a longer path
-        return Math.max(leftLength, rightLength) + 1;//return length of the path
+/**
+ While calculating the depth of tree also keep track of diameter (left tree length + right tree length)
+ Use a global variable to keep track of diameter and bottom up to calculate depth of tree
+  */
+var diameterOfBinaryTree = function (root) {
+  let len = 0;
+  const dfs = (root) => {
+    if (!root) {
+      return 0;
     }
-    dfs(root);
-    return len;
+    const leftLength = dfs(root.left); //Get left path length
+    const rightLength = dfs(root.right); //Get right path length
+    len = Math.max(len, leftLength + rightLength); //see if we found a longer path
+    return Math.max(leftLength, rightLength) + 1; //return length of the path
+  };
+  dfs(root);
+  return len;
 };
