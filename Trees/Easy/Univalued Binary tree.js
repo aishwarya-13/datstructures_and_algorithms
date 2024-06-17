628d9ededda4041f1fbe2e65286db59797ad21a0
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-16 17:04:53 -0700

    Univalued BT

--- Trees/Easy/Univalued Binary tree.js
+++ Trees/Easy/Univalued Binary tree.js
@@ -1,5 +1,42 @@
 //https://leetcode.com/problems/univalued-binary-tree/
 
+/**
+ Passing a variable in dfs to keep track of uni value
+  */
+var isUnivalTree = function (root) {
+  const val = root.val;
+  const dfs = (node, isUnival) => {
+    if (!node) {
+      return true;
+    }
+    isUnival = node.val === val;
+    if (isUnival === false) {
+      return false;
+    }
+    return dfs(node.left, isUnival) && dfs(node.right, isUnival);
+  };
+  return dfs(root, true);
+};
+
+/**
+ * Using a global variable to keep track of uni value
+ */
+var isUnivalTree = function (root) {
+  let isUnival = null,
+    val = root.val;
+  const dfs = (node) => {
+    if (!node || isUnival === false) {
+      //need to return false here because we dont want to proceed if false. So return when false
+      return;
+    }
+    isUnival = node.val === val;
+    dfs(node.left);
+    dfs(node.right);
+  };
+  dfs(root);
+  return isUnival;
+};
+
 var isUnivalTree = function (root) {
   const val = root.val;
   const dfs = (node) => {
