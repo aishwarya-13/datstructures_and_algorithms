b6bf2fdee45755cd9a48e3eb5cf1a36284316d34
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-19 20:06:23 -0700

    Merge two BTs

--- Trees/Easy/Merge Two Binary Trees.js
+++ Trees/Easy/Merge Two Binary Trees.js
@@ -2,6 +2,34 @@
  * https://leetcode.com/problems/merge-two-binary-trees/
  */
 
+/**
+    Optimized "self solution"
+    In self solution, I worried about the non-null node when other node was null
+    But, I dont have to worry about that -> 
+    When one node is null, just return the other node (that will include the node's subtree,
+    be it null or has some left or right nodes)
+ */
+var mergeTrees = function (root1, root2) {
+  const dfs = (node1, node2) => {
+    if (!node1 && !node2) {
+      return null;
+    }
+    if (!node1) {
+      //if node1 is null, then return whole node2(that will include node2's subtree, be it null or has some left and right nodes)
+      return node2;
+    }
+    if (!node2) {
+      //if node2 is null, then return whole node1(that will include node1's subtree, be it null or has some left and right nodes)
+      return node1;
+    }
+    const newNode = new TreeNode(node1.val + node2.val);
+    newNode.left = dfs(node1.left, node2.left);
+    newNode.right = dfs(node1.right, node2.right);
+    return newNode;
+  };
+  return dfs(root1, root2);
+};
+
 //Self -  Using new node
 var mergeTrees = function (root1, root2) {
   const dfs = (node1, node2) => {
