69b611312b301abd6c0b075598ff7227e777e5f3
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-19 19:52:16 -0700

    Merge two BTs

--- Trees/Easy/Merge Two Binary Trees.js
+++ Trees/Easy/Merge Two Binary Trees.js
@@ -2,18 +2,40 @@
  * https://leetcode.com/problems/merge-two-binary-trees/
  */
 
-var mergeTrees = function(root1, root2) {
-    if(!root1 && !root2){
-        return null;
+//Self -  Using new node
+var mergeTrees = function (root1, root2) {
+  const dfs = (node1, node2) => {
+    if (!node1 && !node2) {
+      return null;
     }
-    if(!root1){
-        return root2;
+    let val = 0;
+    if (!node1 && node2) {
+      val = node2.val;
+    } else if (!node2 && node1) {
+      val = node1.val;
+    } else {
+      val = node1.val + node2.val;
     }
-    if(!root2){
-        return root1;
-    }
-    const newNode = new TreeNode(root1.val + root2.val);
-    newNode.left = mergeTrees(root1.left, root2.left);
-    newNode.right = mergeTrees(root1.right, root2.right);
+    const newNode = new TreeNode(val);
+    newNode.left = dfs(node1 ? node1.left : null, node2 ? node2.left : null);
+    newNode.right = dfs(node1 ? node1.right : null, node2 ? node2.right : null);
     return newNode;
+  };
+  return dfs(root1, root2);
+};
+
+var mergeTrees = function (root1, root2) {
+  if (!root1 && !root2) {
+    return null;
+  }
+  if (!root1) {
+    return root2;
+  }
+  if (!root2) {
+    return root1;
+  }
+  const newNode = new TreeNode(root1.val + root2.val);
+  newNode.left = mergeTrees(root1.left, root2.left);
+  newNode.right = mergeTrees(root1.right, root2.right);
+  return newNode;
 };
