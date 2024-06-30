8594a6ec2cb411804500219cfbd42eb709d994f5
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-06-30 00:06:19 -0700

    Invert BT

--- Trees/Easy/Invert Binary Tree.js
+++ Trees/Easy/Invert Binary Tree.js
@@ -2,20 +2,49 @@
  * https://leetcode.com/problems/invert-binary-tree/
  */
 
- /**
+/**
+ Self: Using extra variable
+  */
+var invertTree = function (root) {
+  if (!root) {
+    return null;
+  }
+  const newNode = new TreeNode(root.val);
+  newNode.left = invertTree(root.right);
+  newNode.right = invertTree(root.left);
+  return newNode;
+};
+
+/**
+    Self: Without using extra variable
+    Why use const leftNode = root.left?
+    => Bz when root.left = invertTree(root.right) is executed the root.left's orignal value is lost 
+    Ex: in case of node 7 (In example 1) 7's left is replaced with 9 and the original value (6) is lost
+ */
+var invertTree = function (root) {
+  if (!root) {
+    return null;
+  }
+  const leftNode = root.left;
+  root.left = invertTree(root.right);
+  root.right = invertTree(leftNode);
+  return root;
+};
+
+/**
  We need to swap left nodes with right nodes
   */
 
- var invertTree = function(root) {
-    if(root === null){
-        return null;
-    }
-    //swap
-    let t = root.left;
-    root.left = root.right;
-    root.right = t;
-    //recursive calls
-    invertTree(root.left);
-    invertTree(root.right);
-    return root;
+var invertTree = function (root) {
+  if (root === null) {
+    return null;
+  }
+  //swap
+  let t = root.left;
+  root.left = root.right;
+  root.right = t;
+  //recursive calls
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
 };
