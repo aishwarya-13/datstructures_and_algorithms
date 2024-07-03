f3d1b1208026cc3df4a961f7d824c4878a2a4269
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-07-02 22:57:57 -0700

    Subtree of another tree

--- Trees/Easy/Subtree of Another Tree.js
+++ Trees/Easy/Subtree of Another Tree.js
@@ -2,22 +2,35 @@
  * https://leetcode.com/problems/subtree-of-another-tree/
  */
 
-var isSubtree = function(root, subRoot) {
-    if(!root){
-        return false;
+/**
+ Self: When root's val becomes equal to subRoot's val then check if both 
+ of the nodes (node at val equal to subroot and subroot) is same tree
+  */
+var isSubtree = function (root, subRoot) {
+  if (!root) {
+    return false;
+  }
+  /**
+   * My mistake: i did return isSameTree(root, subRoot). Due to which it didnt check for any node equal to subroot's node after its first encounter
+   * What is same node val is present further down in the tree?
+   * Thats why only return true when isSameTree(root, subRoot) ahs returned true
+   */
+  if (root.val === subRoot.val) {
+    if (isSameTree(root, subRoot)) {
+      return true;
     }
-    return isSameTree(root, subRoot) || subRoot(root.left, subRoot) ||
-            isSameTree(root.right, subRoot);
+  }
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 };
 
-const isSameTree = (p, q)=>{
-    if(!p && !q){
-        return true;
-    }
-    if(!p || !q || p.val !== q.val){
-        return false;
-    }
-    const left = isSameTree(p.left, q.left);
-    const right = isSameTree(p.right, q.right);
-    return left && right && root.val;
-}
+const isSameTree = (root1, root2) => {
+  if (!root1 && !root2) {
+    return true;
+  }
+  if ((!root1 && root2) || (!root2 && root1) || root1.val !== root2.val) {
+    return false;
+  }
+  return (
+    isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right)
+  );
+};
