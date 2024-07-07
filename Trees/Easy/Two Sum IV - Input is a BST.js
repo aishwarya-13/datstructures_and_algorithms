c7962950798b49cccbec6ab5e88f78a5f9bc9386
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-07-06 21:42:42 -0700

    Two Sum IV

--- Trees/Easy/Two Sum IV - Input is a BST.js
+++ Trees/Easy/Two Sum IV - Input is a BST.js
@@ -3,17 +3,39 @@
  */
 
 //Use the same logic as array two sum (Using map)
-var findTarget = function(root, k) {
-    let map = {};
-    const dfs = (root)=>{
-        if(!root){
-            return false;
-        }
-        if(map[root.val] !== undefined){
-            return true;
-        }
-        map[k-root.val] = (k-root.val);
-        return dfs(root.left) || dfs(root.right);
-    };
-    return dfs(root);
+var findTarget = function (root, k) {
+  let map = {};
+  const dfs = (root) => {
+    if (!root) {
+      return false;
+    }
+    if (map[root.val] !== undefined) {
+      return true;
+    }
+    map[k - root.val] = k - root.val;
+    return dfs(root.left) || dfs(root.right);
+  };
+  return dfs(root);
+};
+
+/**
+ Using map just like Two Sum of array
+  */
+var findTarget = function (root, k) {
+  let map = {};
+  const dfs = (node) => {
+    if (!node) {
+      return false;
+    }
+    let diff = k - node.val;
+    if (map[diff] === undefined) {
+      map[node.val] = diff;
+    } else {
+      if (diff + node.val === k) {
+        return true;
+      }
+    }
+    return dfs(node.left) || dfs(node.right);
+  };
+  return dfs(root);
 };
