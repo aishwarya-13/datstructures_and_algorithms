8bb3d316c4f39336cb915acb189ab8dcaad245e7
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-07-07 21:44:43 -0700

    Covert sorted array to BST

--- Trees/Easy/Convert Sorted Array to Binary Search Tree.js
+++ Trees/Easy/Convert Sorted Array to Binary Search Tree.js
@@ -2,23 +2,23 @@
  * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
  */
 
- /**
- Use binary search
+/**
+ Need to create tree recursively (Iterative will not work like you attempted)
+ We need to construct a BST and since the array is sorted, 
+ the mid will be the root and from mid's left will become left subtre and from mid's right will become right subtree
+ And this needs to be done recursively
   */
- var sortedArrayToBST = function(nums) {
-    return createTree(0, nums.length-1, nums);
+var sortedArrayToBST = function (nums) {
+  return createTree(nums, 0, nums.length - 1);
 };
 
-const createTree = (start, end, nums)=>{
-    if(start === end){
-        return new TreeNode(nums[start]);
-    }
-    if(start > end){
-        return null;
-    }
-    let mid = start + Math.floor((end - start) / 2);//get mid in the array
-    let newNode = new TreeNode(nums[mid]);//create new node with mid value in the array
-    newNode.left = createTree(start, mid-1, nums);//create left subtree for the new node
-    newNode.right = createTree(mid+1, end, nums);//create right subtree for the new node
-    return newNode;
-}
+const createTree = (nums, left, right) => {
+  if (left > right) {
+    return null;
+  }
+  const mid = left + Math.floor((right - left) / 2); //get mid in the array
+  const node = new TreeNode(nums[mid], null, null); //create new node with mid value in the array
+  node.left = createTree(nums, left, mid - 1); //create left subtree for the new node
+  node.right = createTree(nums, mid + 1, right); //create right subtree for the new node
+  return node;
+};
