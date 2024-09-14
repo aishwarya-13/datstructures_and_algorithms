a13f21ecdbd4178459da5194c9e85d5cd93e4858
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-09-14 16:44:19 -0700

    Leaf similar trees

--- Trees/Easy/Leaf Similar trees.js
+++ Trees/Easy/Leaf Similar trees.js
@@ -5,6 +5,31 @@
  We cannot traverse both the trees simultaneously because depth of both trees are different
   */
 var leafSimilar = function (root1, root2) {
+  const leafSeq1 = getLeafSeq(root1);
+  console.log("leafSeq1", leafSeq1);
+  const leafSeq2 = getLeafSeq(root2);
+  console.log("leafSeq2", leafSeq2);
+  return leafSeq1 === leafSeq2;
+};
+
+const getLeafSeq = (node) => {
+  if (!node) {
+    return "";
+  }
+  if (!node.left && !node.right) {
+    return node.val;
+  }
+  const left = getLeafSeq(node.left);
+  const right = getLeafSeq(node.right);
+  let str = left && right ? `${left}->${right}` : left && !right ? left : right;
+  return str;
+};
+
+/**
+ Brute force
+ We cannot traverse both the trees simultaneously because depth of both trees are different
+  */
+var leafSimilar = function (root1, root2) {
   const root1Leaves = getLeavesLeftToRight(root1);
   console.log("root1Leaves", root1Leaves);
   const root2Leaves = getLeavesLeftToRight(root2);
