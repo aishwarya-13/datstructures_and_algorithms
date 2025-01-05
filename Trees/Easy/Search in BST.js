761ca478e975adbe97196d7079db04a80d343484
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-04 18:45:52 -0800

    Search in BST

--- Trees/Easy/Search in BST.js
+++ Trees/Easy/Search in BST.js
@@ -1,7 +1,28 @@
 //https://leetcode.com/problems/search-in-a-binary-search-tree/description/
 
 /**
- Self: Taking advantage of the classic binay search
+ * Consider Ex: [4,2,7,1,3] val = 2
+ */
+var searchBST = function (root, val) {
+  if (!root) {
+    return null;
+  }
+  if (root.val === val) {
+    return root;
+  } else if (val < root.val) {
+    /**
+     * When control returns from line "return root", it returns the node.
+     * So we need a return statement here. Else, undefined will be returned as an answer
+     */
+    const node = searchBST(root.left, val);
+    return node;
+  } else {
+    return searchBST(root.right, val);
+  }
+};
+
+/**
+ Self: Taking advantage of the classic binary search
  If val < node.val then go left
  If val > node.val then go right
  If val === node.val we got the answer
