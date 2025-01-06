e088ddf7a61f7d11a5ee06f5fbc7f46782c28e3b
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-05 18:46:42 -0800

    Closest BST value

--- Trees/Easy/Closest Binary Search Tree Value.js
+++ Trees/Easy/Closest Binary Search Tree Value.js
@@ -1,9 +1,41 @@
 /**
  *https://leetcode.com/problems/closest-binary-search-tree-value/description/
  */
+
+/**
+ * Taking advantage of Binary search
+ */
+var closestValue = function (root, target) {
+  let min = +Infinity,
+    result = +Infinity;
+  const dfs = (node) => {
+    if (!node) {
+      return;
+    }
+    let diff = Math.abs(node.val - target);
+    if (diff < min) {
+      min = diff;
+      result = node.val;
+    }
+    if (diff === min) {
+      //if diff is same as previous min then get the closest node
+      result = Math.min(result, node.val);
+    }
+    if (target >= node.val) {
+      dfs(node.right);
+    }
+    if (target <= node.val) {
+      dfs(node.left);
+    }
+  };
+  dfs(root);
+  return result;
+};
+
 /**
  Aishwarya
  Self
+ Without taking advantage of binary search
   */
 var closestValue = function (root, target) {
   let min = +Infinity,
