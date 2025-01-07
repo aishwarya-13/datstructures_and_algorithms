52bff304656aaf14f8c041d50628bbe2b5476795
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-07 15:53:44 -0800

    Binary tree tilt

--- Trees/Easy/Binary Tree Tilt.js
+++ Trees/Easy/Binary Tree Tilt.js
@@ -2,17 +2,18 @@
  * https://leetcode.com/problems/binary-tree-tilt/
  */
 
-var findTilt = function(root) {
-    let tilt = 0;
-    const dfs = (root)=>{
-        if(!root){
-            return 0;
-        }
-        const leftSum = dfs(root.left);
-        const rightSum = dfs(root.right);
-        tilt += Math.abs(leftSum - rightSum)
-        return leftSum + rightSum + root.val;
+var findTilt = function (root) {
+  let tilt = 0;
+  const dfs = (root) => {
+    if (!root) {
+      return 0;
     }
-    dfs(root);
-    return tilt;
+    const leftSum = dfs(root.left);
+    const rightSum = dfs(root.right);
+    tilt += Math.abs(leftSum - rightSum);
+    //need to return all these three values because for a node we need its left subtree's and right subtree's sum
+    return leftSum + rightSum + root.val;
+  };
+  dfs(root);
+  return tilt;
 };
