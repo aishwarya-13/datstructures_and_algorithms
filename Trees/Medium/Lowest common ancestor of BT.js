1c6f0ad572ed1bc1cfb1d74ed0c69ecb8d81ce64
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-07 20:28:36 -0800

    Lowest common ancestor of BT

--- Trees/Medium/Lowest common ancestor of BT.js
+++ Trees/Medium/Lowest common ancestor of BT.js
@@ -2,6 +2,17 @@
 
 /**
  To be done before interview
+ If one node lies in the left subtree and the other in the right subtree, the current root is the LCA.
+ If both nodes lie in the same subtree, the LCA will be in that subtree.
+  */
+/**
+If left node is null then p&q are in the right subtree
+If right node is null then p&q are in left subtree
+If both left and right are present then the node itself is the ancestor
+
+Note: Diagram has been drawn in the notebook. See notebook for visuals */
+/**
+ To be done before interview
   */
 var lowestCommonAncestor = function (root, p, q) {
   if (!root || root === p || root === q) {
