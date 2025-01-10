0322fe99c543d22cbeeb5e4975c00aeb19cbc85f
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-09 20:27:18 -0800

    Deepest leaves sum

--- Trees/Medium/Deepest Leaves Sum.js
+++ Trees/Medium/Deepest Leaves Sum.js
@@ -3,6 +3,36 @@
  */
 
 /**
+   Using O(1) space
+   Maintain a var to keep track of max depth
+   When a leaf node is reached, check if current depth is > max depth
+   If yes then update max depth and replace sum with current node value
+   If max depth is equal to current depth then add to the sum 
+ */
+
+var deepestLeavesSum = function (root) {
+  let sum = 0,
+    maxDepth = 0;
+  const dfs = (node, depth) => {
+    if (!node) {
+      return;
+    }
+    if (!node.left && !node.right) {
+      if (depth > maxDepth) {
+        maxDepth = depth;
+        sum = node.val;
+      } else if (maxDepth === depth) {
+        sum += node.val;
+      }
+    }
+    dfs(node.left, depth + 1);
+    dfs(node.right, depth + 1);
+  };
+  dfs(root, 1);
+  return sum;
+};
+
+/**
  We need to find deepest leaves i.e those which are present at lowest level
  This problem can be broken into of two problems
  1. Max depth of BT
