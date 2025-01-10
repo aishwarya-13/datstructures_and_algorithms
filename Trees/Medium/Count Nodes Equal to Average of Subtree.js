8f3da5fd5fe4470b8b3eed82555a7f2000bf7084
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-09 20:58:00 -0800

    Count nodes = to avg of subtree

--- Trees/Medium/Count Nodes Equal to Average of Subtree.js
+++ Trees/Medium/Count Nodes Equal to Average of Subtree.js
@@ -1,6 +1,32 @@
 /**
  *https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/description/
  */
+var averageOfSubtree = function (root) {
+  let resultCount = 0;
+  const dfs = (node, count, sum) => {
+    if (!node) {
+      return [0, 0];
+    }
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+    const leftSubtreeNodes = left[0];
+    const leftSubtreeNodeSum = left[1];
+    const rightSubtreeNodes = right[0];
+    const rightSubtreeNodeSum = right[1];
+    const subTreeNodesCount = leftSubtreeNodes + rightSubtreeNodes + 1;
+    const subTreeNodeSum = leftSubtreeNodeSum + rightSubtreeNodeSum + node.val;
+
+    const avg = Math.floor(subTreeNodeSum / subTreeNodesCount);
+
+    if (avg === node.val) {
+      resultCount += 1;
+    }
+    return [subTreeNodesCount, subTreeNodeSum];
+  };
+  dfs(root, 1, root.val);
+  return resultCount;
+};
+
 /**
     We reach to the bottom of the tree first and go up calculating the sum of nodes and count of nodes
     in the subtree for that node
