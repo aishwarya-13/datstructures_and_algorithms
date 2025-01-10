dc1ed76b11343cb9edecf71b7860961fb82528fe
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-09 19:28:14 -0800

    Balance BST

--- Trees/Medium/Balance a Binary Search Tree.js
+++ Trees/Medium/Balance a Binary Search Tree.js
@@ -1,6 +1,7 @@
 /**
  * https://leetcode.com/problems/balance-a-binary-search-tree/description/
  */
+
 /**
  Approach:
  1. Inorder traversl will give a sorted list. So, store the tree nodes in sorted order in an array
@@ -13,16 +14,29 @@
  Space: O(N)
   */
 var balanceBST = function (root) {
-  const sortedTreeNodes = inorderTraversal(root);
-  console.log("sortedTreeNodes", sortedTreeNodes);
-  return buildTree(sortedTreeNodes, 0, sortedTreeNodes.length - 1);
+  const inorderArr = inorder(root);
+  return buildTree(inorderArr, 0, inorderArr.length - 1);
 };
 
-const inorderTraversal = (root) => {
-  if (!root) {
+const inorder = (node) => {
+  if (!node) {
     return [];
   }
-  const left = inorderTraversal(root.left);
-  const right = inorderTraversal(root.right);
-  return [...left, root, ...right];
+  const left = inorder(node.left);
+  const right = inorder(node.right);
+  //here instead of returning node's value return node itself
+  //so that there is no need to create a new node when building the tree
+  //we modify the exisiting tree itself
+  return [...left, node, ...right];
+};
+
+const buildTree = (inorderArr, left, right) => {
+  if (left > right) {
+    return null;
+  }
+  let mid = left + Math.floor((right - left) / 2);
+  const node = inorderArr[mid];
+  node.left = buildTree(inorderArr, left, mid - 1);
+  node.right = buildTree(inorderArr, mid + 1, right);
+  return node;
 };
