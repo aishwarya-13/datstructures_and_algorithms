392dbfbe42b799e3edf0906c44e7f9d1d7d28455
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-11 20:59:18 -0800

    Find left most value

--- Trees/Medium/Find Bottom left value.js
+++ Trees/Medium/Find Bottom left value.js
@@ -1,32 +1,31 @@
 //https://leetcode.com/problems/find-bottom-left-tree-value/
 
-/** 
-The description is misleading 
-Leftmost value does not necessarily mean left child of last row 
-We need to find the node value of the last row. Duh! 
-*/
+/**
+ The description is not fully explanatory
+ More explanation: 
+ Leftmost node does not mean left child. Leftmost means left side of the tree
+ Return the left most node at last row means
+ 1. If at last row all non null nodes are present at same level, then return the leftmost node
+ 2. If at last row only right child is present and not left child that means they want that right child to be returned. In this case leftmost node is that right child
 
+Since left side of tree needs to be traversed we will do preorder traversal
+Will visit the left subtree first (since leftmost node is asked) then right subtree
+  */
 var findBottomLeftValue = function (root) {
   let lowestLeftNode = root.val,
     maxLevel = 1;
-
   const dfs = (node, currentLevel, isLeft, parentNode) => {
     if (!node) {
       return;
     }
-
     if (currentLevel > maxLevel) {
+      //IMP to update the max level only when the current level is greater than max
       maxLevel = currentLevel;
-
       lowestLeftNode = node.val;
     }
-
-    dfs(node.left, currentLevel + 1);
-
+    dfs(node.left, currentLevel + 1); //since leftmost needs to be returned, visit the left subtree first
     dfs(node.right, currentLevel + 1);
   };
-
   dfs(root, 1);
-
   return lowestLeftNode;
 };
