19d5b91efc87feb073fb3e7fdf5e55b33dedcf8d
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-01-12 19:37:39 -0800

    Binary tree right side view

--- Trees/Medium/Binary tree right side view.js
+++ Trees/Medium/Binary tree right side view.js
@@ -2,6 +2,47 @@
  * https://leetcode.com/problems/binary-tree-right-side-view/description/
  */
 /**
+ If i == levelLength - 1, then it's the last node in the current level, push it to rightsize list.
+ 
+1. result = [], queue = [3], qSize = 1
+    i=0, curr = 1, result = [1], queue=[2,3]
+2. result = [1] queue=[2,3] qSize = 2
+    i=0, curr = 2, queue=[3], result = [1], queue=[3,4]
+    i=1, curr = 3, queue=[4] result = [1, 3] queue=[4]
+3. result = [1, 3] queue=[4], qSize=1
+    i=0 curr=4 queue=[] result=[1,3,4] queue=[5]
+4. result=[1,3,4] queue=[5] qSize=1
+    i=0 curr=5 queue=[] result=[1,3,4,5] queue=[]
+
+    result=[1,3,4,5]
+
+  */
+var rightSideView = function (root) {
+  if (!root) {
+    return [];
+  }
+  let result = [],
+    queue = [root];
+  while (queue.length > 0) {
+    let qSize = queue.length;
+    for (let i = 0; i < qSize; i++) {
+      let curr = queue.shift();
+      if (i === qSize - 1) {
+        result.push(curr.val);
+      }
+      if (curr.left) {
+        queue.push(curr.left);
+      }
+      if (curr.right) {
+        queue.push(curr.right);
+      }
+    }
+  }
+  console.log("result", result);
+  return result;
+};
+
+/**
  Brute force - Self
 1. Iterative level order traversal with one difference -> push right child first instead of left
  This way we have the right side nodes first
