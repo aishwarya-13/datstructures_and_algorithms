294574b7038f4dfbbec11b79f76639b950f55141
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-04-24 20:43:44 +0530

    Search in rotated sorted array

--- Binary Search/Medium/Search in Rotated Sorted Array.js
+++ Binary Search/Medium/Search in Rotated Sorted Array.js
@@ -5,9 +5,26 @@
  */
 
 /**
- Check if from mid if left part or right part is sorted (ascending)
- Then check if target lies within the boundaries
-  */
+Approach 2: Single binary search
+Compare mid with left to determine where to continue searching
+1. If mid > left that means left subarray is sorted
+2. If mid < left that means right subarray is sorted
+
+Now after deciding which subarray to pick proceed to check if target lies within that subarrays boundaries
+If it does not lie then pick the other subarray
+If it does lie then proceed BS in this subarray
+
+Time: O(log N)
+Space: O(1)
+
+If we cut a subarray nums[left ~ right] by the index mid. We split this subarray into 3 parts:
+
+subarray nums[left ~ mid - 1]
+element nums[mid].
+subarray nums[mid + 1, right].
+It is important to note that there is at most one rotated sorted array in the two subarrays, which means that there is 
+at least one sorted array for comparison.
+ */
 var search = function (arr, target) {
   let left = 0,
     right = arr.length - 1;
