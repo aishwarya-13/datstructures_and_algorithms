f60b28dc9d77b9b107a3c11f5e3a37762255f152
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-05-03 12:07:44 +0530

    Assign cookies

--- Two Pointers/Easy/Assign Cookies.js
+++ Two Pointers/Easy/Assign Cookies.js
@@ -1,30 +1,41 @@
 /**
  * https://leetcode.com/problems/assign-cookies/
  */
- /**
-    The cookie should be greater than or equal to greed.
-    So sort both the arrays and compare the elements
-    Time: O(G) number of children - size of greed array
-    Space: O(1)
-    g=[7,8,9,10]
-    c=[5,6,7,8]
-    Time: O(nlogn)
-    Space: O(1)
+/**
+ Mistakes made: 
+ 1. "Assumed the array is sorted". 
+ 2. Did not consider a scenario
+ 3. No need to add else loop for j++. j++ is written in if as well as else loop so remove else
+ 4. && consition in while condition
+
+ Lessons learnt:
+ 1. Don't assume. Ask the interviewer explicitly.
+ 2. Consider all scenarios
+ 3. Try to reduce redundant else loops / code
+ 4. If two arrays are to be iterated see if we can put || && conditions
+
+ Time: O(NlogN + MlogM)
+ N - length of g array
+ W - length of s array
+ sorting takes logN, logM
+ while loop -> O(M + N)
+
+ Space: Sorting is done in-place so O(1)
+ O(1)
   */
-    var findContentChildren = function(greed, cookies) {
-      greed.sort((a,b)=>a-b)
-      cookies.sort((a,b)=>a-b)
-      let g = 0,
-          c = 0,
-          result = 0
-      while(g < greed.length && c < cookies.length){
-          if(cookies[c] >= greed[g]){
-              result++
-              c++
-              g++
-          }else{
-              c++
-          }
-      }
-      return result
-  };
+var findContentChildren = function (g, s) {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  console.log("g", g, "s", s);
+  let contentCount = 0,
+    i = 0,
+    j = 0;
+  while (j < s.length && i < g.length) {
+    if (s[j] >= g[i]) {
+      contentCount++;
+      i++;
+    }
+    j++;
+  }
+  return contentCount;
+};
