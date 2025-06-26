7a7d9ddc327336f19b7bac44891216ae36429d86
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-06-26 13:56:15 -0700

    Valid anagram

--- Arrays and Hashing/Easy/Valid Anagram.js
+++ Arrays and Hashing/Easy/Valid Anagram.js
@@ -2,7 +2,40 @@
  * https://leetcode.com/problems/valid-anagram/
  */
 
- /**
+/**
+    Using a map and two different count variables
+ Time: O(N + M) N is the length of string 's', M is the length of string 't'
+ Space: O(N)
+  */
+var isAnagram = function (s, t) {
+  let map = {},
+    count1 = 0,
+    count2 = 0;
+  for (let char of s) {
+    if (map[char] === undefined) {
+      map[char] = 0;
+      count1++;
+    }
+    map[char]++;
+  }
+  for (let char of t) {
+    if (char in map && map[char] > 0) {
+      //imp to check the frequency count as well else delete the key
+      map[char]--;
+      if (map[char] === 0) {
+        count1--;
+      }
+    } else {
+      count2++;
+    }
+  }
+  if (count1 === 0 && count2 === 0) {
+    return true;
+  }
+  return false;
+};
+
+/**
     Maintain a map to store the character and its frequency.
     Iterate str1 to load the map and calculate the length of map
     Iterate str2:
@@ -14,27 +47,27 @@
     Time: O(N)+(M)
     Space: O(N)
   */
-    const isAnagram = (str1, str2)=>{
-      let map = {},
-          mapLen = 0
-      for(let char of str1){
-          if(map[char] === undefined){
-              map[char] = 0
-              mapLen++
-          }
-          map[char]++
+const isAnagram = (str1, str2) => {
+  let map = {},
+    mapLen = 0;
+  for (let char of str1) {
+    if (map[char] === undefined) {
+      map[char] = 0;
+      mapLen++;
+    }
+    map[char]++;
+  }
+  for (let char of str2) {
+    if (map[char] !== undefined) {
+      map[char]--;
+      if (map[char] === 0) {
+        delete map[char];
+        mapLen--;
       }
-      for(let char of str2){
-          if(map[char] !== undefined){
-              map[char]--
-              if(map[char] === 0){
-                  delete map[char]
-                  mapLen--
-              }
-          }else{
-              map[char] = char
-              mapLen++
-          }
-      }
-      return mapLen === 0
-  };
+    } else {
+      map[char] = char;
+      mapLen++;
+    }
+  }
+  return mapLen === 0;
+};
