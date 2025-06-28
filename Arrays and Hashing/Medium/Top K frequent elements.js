e5152d25491efa862bbb8ad2c57cc92e008fcdcf
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-06-27 18:38:09 -0700

    Top k freq elements

--- Arrays and Hashing/Medium/Top K frequent elements.js
+++ Arrays and Hashing/Medium/Top K frequent elements.js
@@ -2,24 +2,31 @@
  * https://leetcode.com/problems/top-k-frequent-elements/description/
  */
 
- /**
- 1]Count frequency of each ele in a map
- 2]Sort keys in map descending according to 'values'
- 3]Push first k elements of sorted keys in array
+/**
+ Using map
+ 1. Record a number and its frequency in the map
+ 2. Iterate the map and get first k keys from the map
+
+ Time: O(NlogN)
+ Space: O(N)
   */
- var topKFrequent = function(nums, k) {
-  let map = {},
-      result = [];
-  for(let num of nums){
-      if(map[num] === undefined){
-          map[num] = 0
-      }
-      map[num]++
+var topKFrequent = function (nums, k) {
+  let result = [];
+  let map = {};
+  for (let num of nums) {
+    if (map[num] === undefined) {
+      map[num] = 0;
+    }
+    map[num]++;
   }
-  //Sort object keys in descending order according to values
-  let freq = Object.keys(map).sort((a,b)=> map[b] - map[a])
-  for(let i=0; i<k; i++){
-      result.push(freq[i])
+  console.log(map);
+  //let freq = Object.keys(map).map(Number).sort((a,b) => map[b] - map[a])
+  //Object.keys(map).map(str => Number(str)).sort((a,b) => map[b] - map[a])
+  let freq = Object.entries(map).map(([num, freq]) => [freq, parseInt(num)]);
+  console.log(freq);
+  freq.sort((a, b) => b[0] - a[0]);
+  for (let i = 0; i < k; i++) {
+    result.push(freq[i][1]);
   }
-  return result
+  return result;
 };
