8d57d7a8f99e3c4551649cea3a70633997d77956
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-07-04 20:46:28 -0700

    Product of array except self

--- Prefix Sum/Product of Array except Self.js
+++ Prefix Sum/Product of Array except Self.js
@@ -6,6 +6,30 @@
  Using prefix product and sufix product
  [a0, a1, a2, a3, a4, a5, ...., an-1]
  So here a3 is a product of a0 to a2 (left sufix product) and a4, a5 to an-1(right sufix product)
+ We optimize the solution here by using the result array itself for calculation
+
+ Time: O(N)
+ Space: O(1) result array is not counted
+  */
+var productExceptSelf = function (nums) {
+  let result = [],
+    mul = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = mul; //here result array will hold left prefix product
+    mul *= nums[i];
+  }
+  mul = 1; //this variable keeps track of running product from right
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= mul;
+    mul *= nums[i];
+  }
+  return result;
+};
+
+/**
+ Using prefix product and sufix product
+ [a0, a1, a2, a3, a4, a5, ...., an-1]
+ So here a3 is a product of a0 to a2 (left sufix product) and a4, a5 to an-1(right sufix product)
  So, in first iteration calculate left sufix products of all the elements
  In second iteration calculate right sufix product of all the elements
  In third iteration multiply left and right sufix product to get the answer
