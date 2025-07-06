51737ff02df28488c79172ce99385a74fe3fb4eb
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-07-05 17:16:01 -0700

    Contains duplicate II

--- Arrays and Hashing/Easy/Contains Duplicate II.js
+++ Arrays and Hashing/Easy/Contains Duplicate II.js
@@ -2,36 +2,23 @@
  * https://leetcode.com/problems/contains-duplicate-ii/description/
  */
 
-//Solution using Sliding window also exists and is more effecient
-
-const containsNearbyDuplicate = (nums, k) => {
-  let map = {};
-  for (let i = 0; i < nums.length; i++) {
-    if (Math.abs(map[nums[i]] - i) <= k) {
-      return true;
-    }
-    map[nums[i]] = i;
-  }
-  return false;
-};
+//Solution using Sliding window also exists. It is more effecient
 
 /**
-    1] Add ele as key and index as value in map
-    2] If ele exists in map then calculate the distance between the indices and chekc for criteria. If criteria matches then return true. Else replace the current index in the map
+ Time: O(N)
+ Space: O(N)
   */
-const containsNearbyDuplicate = (nums, k) => {
+var containsNearbyDuplicate = function (nums, k) {
   let map = {};
   for (let i = 0; i < nums.length; i++) {
-    if (map[nums[i]] === undefined) {
-      map[nums[i]] = i;
-    } else {
-      let diff = Math.abs(map[nums[i]] - i);
+    if (map[nums[i]] !== undefined) {
+      let index = map[nums[i]],
+        diff = Math.abs(index - i);
       if (diff <= k) {
         return true;
-      } else {
-        map[nums[i]] = i;
       }
     }
+    map[nums[i]] = i; //update the index if an ele occurs more than once
   }
   return false;
 };
