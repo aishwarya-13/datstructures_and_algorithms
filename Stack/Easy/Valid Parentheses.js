db3d480d0aa4a216e13afe49e4e6932f89438421
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-07-08 16:08:22 -0700

    Valid parentheses

--- Stack/Easy/Valid Parentheses.js
+++ Stack/Easy/Valid Parentheses.js
@@ -6,19 +6,21 @@
     Time: O(N) - We traverse the string one character at a time and push, pop and peek operations are O(1)
     Space: O(N) 
  */
-    var isValid = (str)=>{
-        let stack = [];
-        for(let i=0; i<str.length; i++){
-            if(str[i] === ')' && stack[stack.length - 1] !== '(' ||
-                str[i] === ']' && stack[stack.length - 1] !== '[' ||
-                str[i] === '}' && stack[stack.length - 1] !== '{'){
-                    stack.pop()
-            }else{
-                stack.push(str[i])
-            }
-        }
-        return stack.length === 0
-    };
+var isValid = (str) => {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (
+      (str[i] === ")" && stack[stack.length - 1] !== "(") ||
+      (str[i] === "]" && stack[stack.length - 1] !== "[") ||
+      (str[i] === "}" && stack[stack.length - 1] !== "{")
+    ) {
+      stack.pop();
+    } else {
+      stack.push(str[i]); //imp to push any string onto the stack. DO NOT put this condition in if loop
+    }
+  }
+  return stack.length === 0;
+};
 
 /**
     Push opening brace onto stack
@@ -26,20 +28,22 @@
     Time: O(N)
     Space: O(N)
  */
-var isValid = (str)=>{
-    let stack = [];
-    for(let c of str){
-        if(c === '(' || c === '[' || c === '{'){
-            stack.push(c);
-        }
-        if((c === ')' && stack.pop() !== '(') ||
-            (c === ']' && stack.pop() !== '[') ||
-            (c === '}' && stack.pop() !== '{')){
-                return false;
-        }
+var isValid = (str) => {
+  let stack = [];
+  for (let c of str) {
+    if (c === "(" || c === "[" || c === "{") {
+      stack.push(c);
     }
-    if(stack.length === 0){
-        return true;
+    if (
+      (c === ")" && stack.pop() !== "(") ||
+      (c === "]" && stack.pop() !== "[") ||
+      (c === "}" && stack.pop() !== "{")
+    ) {
+      return false;
     }
-    return false;
+  }
+  if (stack.length === 0) {
+    return true;
+  }
+  return false;
 };
