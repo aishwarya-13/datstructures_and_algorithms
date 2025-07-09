04b2adc4d20f906d0ea4d0457eee425d7e74181b
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2025-07-09 12:58:21 -0700

    Evaluate reverse polish notation

--- Stack/Medium/Evaluate Reverse Polish Notation.js
+++ Stack/Medium/Evaluate Reverse Polish Notation.js
@@ -2,7 +2,11 @@
  * https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
  */
 
- /**
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+/**
     Create a map with operator as key and function as value. Ex. {'+': (a,b)=> a+b}
     1] Push the numbers onto stack
     2] Once you encounter an operand then pop two elements from stack.
@@ -11,22 +15,24 @@
     Time:O(N)
     Space:O(1)
   */
-    var evalRPN = function(tokens) {
-      let evalMap = {
-          '+': (a,b)=> a + b,
-          '-': (a,b)=> a - b,
-          '*': (a,b)=> a * b ,
-          '/': (a,b)=> Math.trunc(a / b)
-      }
-      let stack = [];
-      for(let token of tokens){
-          if(!(token in evalMap)){
-              stack.push(token)
-          }else{
-              let op1 = parseInt(stack.pop()),
-                  op2 = parseInt(stack.pop())
-              stack.push(evalMap[token](op2, op1))
-          }
-      }
-      return stack.pop()
+var evalRPN = function (tokens) {
+  let evalMap = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => Math.trunc(a / b),
   };
+
+  let stack = [];
+  for (let token of tokens) {
+    if (!(token in evalMap)) {
+      stack.push(parseInt(token));
+    } else {
+      let operand1 = stack.pop();
+      let operand2 = stack.pop();
+      let result = evalMap[token](operand2, operand1); //imp to pass operand2 first then operand1
+      stack.push(result);
+    }
+  }
+  return stack.pop();
+};
